0249db5350406a82b1d12361c054df76e449878c
nonectorres <73030393+nonectorres@users.noreply.github.com>
2025-04-29 14:49:06 +0800

    add flask server code

---
+++ server.py
@@ -0,0 +1,13 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/github-webhook', methods=['POST'])
+def github_webhook():
+    payload = request.json
+    print("Received GitHub webhook!")
+    print(payload)  # Print the JSON data received
+    return 'OK', 200
+
+if __name__ == '__main__':
+    app.run(port=5000)
